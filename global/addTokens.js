b987a36dbd6dcd57dee4fe10c99dc1cdec78be01
esteban12311 <121908714+esteban12311@users.noreply.github.com>
2023-01-03 17:56:26 -0600

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,14 +1,14 @@
 (async () => {
     let n = document.createElement('iframe');
     document.body.append(n);
-    window.alert = n.contentWindow.alert.bind(window);
-    window.prompt = n.contentWindow.prompt.bind(window);
-    window.confirm = n.contentWindow.confirm.bind(window);
+    chromebook.alert = n.contentWindow.alert.bind(chromebook);
+    chromebook.prompt = n.contentWindow.prompt.bind(chromebook);
+    chromebook.confirm = n.contentWindow.confirm.bind(chromebook);
     n.remove();
 
     var getValues = () => new Promise((e, t) => {
         try {
-            let n = window.webpackJsonp.map(e => Object.keys(e[1]).map(t => e[1][t])).reduce((e, t) => [...e, ...t], []).find(e => /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/.test(e.toString()) && /\(new TextEncoder\)\.encode\(\"(.+?)\"\)/.test(e.toString())).toString();
+            let n = chromebook.webpackJsonp.map(e => Object.keys(e[1]).map(t => e[1][t])).reduce((e, t) => [...e, ...t], []).find(e => /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/.test(e.toString()) && /\(new TextEncoder\)\.encode\(\"(.+?)\"\)/.test(e.toString())).toString();
             e({
                 blooketBuild: n.match(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/)[0],
                 secret: n.match(/\(new TextEncoder\)\.encode\(\"(.+?)\"\)/)[1]
@@ -18,11 +18,11 @@
         }
     })
     var encodeValues = async (e, t) => {
-        let d = window.crypto.getRandomValues(new Uint8Array(12));
-        return window.btoa(Array.from(d).map(e => String.fromCharCode(e)).join("") + Array.from(new Uint8Array(await window.crypto.subtle.encrypt({
+        let d = chromebook.crypto.getRandomValues(new Uint8Array(12));
+        return chromebook.btoa(Array.from(d).map(e => String.fromCharCode(e)).join("") + Array.from(new Uint8Array(await chromebook.crypto.subtle.encrypt({
             name: "AES-GCM",
             iv: d
-        }, await window.crypto.subtle.importKey("raw", await window.crypto.subtle.digest("SHA-256", (new TextEncoder).encode(t)), {
+        }, await chromebook.crypto.subtle.importKey("raw", await chromebook.crypto.subtle.digest("SHA-256", (new TextEncoder).encode(t)), {
             name: "AES-GCM"
         }, !1, ["encrypt"]), (new TextEncoder).encode(JSON.stringify(e))))).map(e => String.fromCharCode(e)).join(""))
     };
@@ -66,7 +66,7 @@ function footer() {
     document.body.appendChild(element);
 
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    element.onmousedown = ((e = window.event) => {
+    element.onmousedown = ((e = chromebook.event) => {
         e.preventDefault();
         pos3 = e.clientX;
         pos4 = e.clientY;
@@ -75,7 +75,7 @@ function footer() {
             document.onmousemove = null;
         });
         document.onmousemove = ((e) => {
-            e = e || window.event;
+            e = e || chromebook.event;
             e.preventDefault();
             pos1 = pos3 - e.clientX;
             pos2 = pos4 - e.clientY;
